4682629d8eafb8b80f25dde7997cd56bdd783e41
Sefukamil20R <Tenzilk20@gmail.com>
2025-05-07 11:22:59 +0300

    UI update on home page

--- lib/feature/home/presentation/widget/specialties_section.dart
+++ lib/feature/home/presentation/widget/specialties_section.dart
@@ -4,23 +4,40 @@ class SpecialtyCard extends StatelessWidget {
   final String image;
   final String label;
 
-  const SpecialtyCard({required this.image, required this.label, super.key});
+  const SpecialtyCard({
+    required this.image,
+    required this.label,
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        ClipRRect(
-          borderRadius: BorderRadius.circular(12),
-          child: Image.asset(
-            image,
-            width: 70,
-            height: 55,
-            fit: BoxFit.cover,
+        Container(
+          width: 65,
+          height: 53,
+          decoration: BoxDecoration(
+            color: const Color(0xFFEFF9FF),
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: Center(
+            child: Image.asset(
+              image,
+              width: 36,
+              height: 36,
+              fit: BoxFit.contain,
+            ),
           ),
         ),
-        const SizedBox(height: 6),
-        Text(label, style: const TextStyle(fontSize: 12)),
+        const SizedBox(height: 8),
+        Text(
+          label,
+          style: const TextStyle(
+            fontSize: 12,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
       ],
     );
   }
